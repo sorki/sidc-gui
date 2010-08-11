9759eaa1da1820ffb2771119a995d0d494b75e04
Richard Marko <rissko@gmail.com>
2010-08-11 15:33:28 +0200

    Switched to psutil instead of parsing ps output

--- core/threads.py
+++ core/threads.py
@@ -1,7 +1,7 @@
 import os
 import wx
+import psutil
 import logging
-import commands
 import datetime
 import threading
 
@@ -9,7 +9,7 @@ EVT_RESULT_ID = wx.NewId()
 
 class ResultEvent(wx.PyEvent):
     ''' Event to carry arbitrary payload '''
-    def __init__(self, data):
+    def __init__(self, data=(None,None)):
         super(ResultEvent, self).__init__()
         self.SetEventType(EVT_RESULT_ID)
         self.data = data
@@ -39,84 +39,89 @@ class WxThread(threading.Thread):
         
 class InfoThread(WxThread):
     def run(self):
-        # keep args at the end as
-        ps_format = 'comm,pid,user,time,%cpu,%mem,' \
-            'policy,nice,etime,args'
-        out = commands.getstatusoutput('ps ax -o %s | grep ^sidc' % ps_format)
-        out_start = commands.getoutput('ps ax -o comm,lstart | grep ^sidc')
-        
+        sidc_list = filter(lambda p: p.name == 'sidc', psutil.process_iter())
 
-        sidc_status = {}
-
-        if out[0] == 0:
-            logging.debug('Found task matching \'sidc\'')
-            sidc_status['start_time'] =  datetime.datetime.strptime(
-                 out_start[4:].strip(), '%a %b  %d %H:%M:%S %Y')
-            sproc = out[1].strip().split()
-
-            ps_items = ps_format.split(',')
-            for i, item in enumerate(ps_items):
-                sidc_status[item] = (
-                    sproc[i] if i+1 != len(ps_items) else ' '.join(sproc[i:]))
-
-
-            out = commands.getstatusoutput(
-                'cd /proc/%s/cwd; pwd -P' % sidc_status['pid'])
-            fail = True
-            if out[0] == 0:
-                sidc_status['dir'] = out[1]
-
-            # find config 
-            
-            # default location
-            cfg_file = '/etc/sidc.conf'
-            if os.path.exists(cfg_file):
-                sidc_status['cfg_file'] = cfg_file
-                logging.debug('Default sidc config file found')
-
-            # custom location
-            find_result = sidc_status['args'].find('sidc ')
-            if find_result != -1:
-                params = sidc_status['args'][find_result+5:]
-                pos = params.find('c ') 
-                if pos != -1:
-                    filename = params[pos+2:].split(' ')[0]
-                    cfg_file = os.path.join(sidc_status['dir'], filename)
-                    if os.path.exists(cfg_file):
-                        sidc_status['cfg_file'] = cfg_file
-                        logging.debug('Custom sidc config file found')
-
-            if 'cfg_file' not in sidc_status:
-                logging.warning('Sidc is running but no config file found')
-            else:
-                if os.access(sidc_status['cfg_file'], os.R_OK):
-                    # config ok, read & parse
-                    cfg_contents = open(sidc_status['cfg_file']).readlines()
-                    cfg_contents = filter(lambda x: len(x)>0 and x[0] != ';',
-                        map(lambda x: x.strip(), cfg_contents))
-
-                    bands = {}
-                    for item in cfg_contents:
-                        parts = map(lambda x: x.strip(), item.split())
-                        i = 1
-                        while parts[i] != ';':
-                            i += 1
-                            if i == len(parts):
-                                break
-
-                        if parts[0] == 'band':
-                            bands[parts[1]] = parts[2:i]
-                        else:
-                            # if options expects multiple args save them as list
-                            sidc_status[parts[0]] = parts[1:i] if i>2 else parts[1]
-
-
-                    sidc_status['bands'] = bands
-                else:
-                    logging.warning('Unable to read config file. Check permissions')
-
-            self.result(sidc_status)
+        if len(sidc_list)==0:
+            logging.info('No sidc instance found. Live data disabled')
+            self.result()
+            return
+        elif len(sidc_list)>1:
+            logging.warning('Multiple sidc instances found - not supported')
+            self.result()
             return
 
-        logging.info('No sidc instance found. Live data disabled')
-        self.result(None)
+        sidc = sidc_list[0]
+
+        sidc_status = {}
+        attrs = '''
+            pid path cmdline uid gid username create_time
+            get_cpu_percent get_memory_percent
+            '''
+
+        for i in attrs.split():
+            attrib = getattr(sidc, i)
+            key = i.replace('get_', '')
+            sidc_status[key] = attrib() if callable(attrib) else attrib
+
+        sidc_status['create_datetime'] = datetime.datetime.fromtimestamp(
+            sidc_status['create_time'])
+
+        (sidc_status['cpu_user'],
+            sidc_status['cpu_system']) = sidc.get_cpu_times()
+        (sidc_status['mem_resident'],
+            sidc_status['mem_virtual']) = sidc.get_memory_info()
+
+        # find config
+
+        # default location
+        cfg_file = '/etc/sidc.conf'
+        if os.path.exists(cfg_file):
+            sidc_status['cfg_file'] = cfg_file
+            logging.debug('Default sidc config file found')
+
+        # custom location
+        cmdline = ' '.join(sidc_status['cmdline'])
+        find_result = cmdline.find('sidc ')
+        if find_result != -1:
+            params = cmdline[find_result+5:]
+            pos = params.find('c ')
+            if pos != -1:
+                filename = params[pos+2:].split(' ')[0]
+                cfg_file = os.path.join(sidc_status['path'], filename)
+                if os.path.exists(cfg_file):
+                    sidc_status['cfg_file'] = cfg_file
+                    logging.debug('Custom sidc config file found')
+
+        if 'cfg_file' not in sidc_status:
+            logging.warning('Sidc is running but no config file found')
+        else:
+            if os.access(sidc_status['cfg_file'], os.R_OK):
+                # config ok, read & parse
+                cfg_contents = open(sidc_status['cfg_file']).readlines()
+                cfg_contents = filter(lambda x: len(x)>0 and x[0] != ';',
+                    map(lambda x: x.strip(), cfg_contents))
+
+                bands = {}
+                cfg = {}
+                for item in cfg_contents:
+                    parts = map(lambda x: x.strip(), item.split())
+                    i = 1
+                    while parts[i] != ';':
+                        i += 1
+                        if i == len(parts):
+                            break
+
+                    if parts[0] == 'band':
+                        bands[parts[1]] = parts[2:i]
+                    else:
+                        # if options expects multiple args save them as list
+                        cfg[parts[0]] = parts[1:i] if i>2 else parts[1]
+
+
+                cfg['bands'] = bands
+            else:
+                logging.warning('Unable to read config file. Check permissions')
+
+        self.result((sidc_status, cfg))
+        return
+
