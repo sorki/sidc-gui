baa2388eb9845dbd05b2a9bcca9e0d8a1633fa4d
Richard Marko <rissko@gmail.com>
2013-02-04 15:16:58 +0100

    Bump version, update changelog

--- sidc_gui/__init__.py
+++ sidc_gui/__init__.py
@@ -1,11 +1,10 @@
 __author__ = 'Richard Marko <rissko@gmail.com>'
 __status__ = 'beta'
-__version__ = '0.3'
+__version__ = '0.4'
 
 __title__ = 'Sidc GUI'
-__copyright__ = '(c) 2010-2012 Richard Marko'
+__copyright__ = '(c) 2010-2013 Richard Marko'
 __website__ = 'http://github.com/sorki/sidc-gui'
 
 __description__ = 'Sudden ionospheric disturbance colletor (sidc) GUI.'
-__license__ = '''This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
-To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/'''
+__license__ = '''This work is licensed under the BSD license.'''
